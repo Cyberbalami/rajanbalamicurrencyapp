2e39dbde6834ee55f270da95446fc575e1ad7c6c
Rajan Balami <1796826@activedir.vu.lt>
2025-11-26 09:44:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rajanbalamicurrencyapp/parsers/JsonParser.java
@@ -0,0 +1,4 @@
+package com.example.rajanbalamicurrencyapp.parsers;
+
+public class JsonParser {
+}
---
+++ app/src/main/java/com/example/rajanbalamicurrencyapp/utilities/ApiDataReader.java
@@ -0,0 +1,4 @@
+package com.example.rajanbalamicurrencyapp.utilities;
+
+public class ApiDataReader {
+}
